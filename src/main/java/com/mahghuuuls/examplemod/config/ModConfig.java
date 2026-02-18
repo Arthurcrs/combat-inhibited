5c2e9da6071c9ab626fd2f8988f980fbf9421633
Arthur Carlos <arthcrs@gmail.com>
2026-02-18 20:09:05 -0300

    Initial commit.

---
+++ src/main/java/com/mahghuuuls/examplemod/config/ModConfig.java
@@ -0,0 +1,13 @@
+package com.mahghuuuls.examplemod.config;
+
+import com.mahghuuuls.examplemod.ExampleMod;
+
+import net.minecraftforge.common.config.Config;
+
+@Config(modid = ExampleMod.MOD_ID)
+public class ModConfig {
+
+//	@Config.Comment("Example config")
+//	public static boolean blockMelee = true;
+
+}
