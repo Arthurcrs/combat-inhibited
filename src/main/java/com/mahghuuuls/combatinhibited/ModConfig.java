839b4c00a92e420193f39afc721027ad9b22e92c
Arthur Carlos <arthcrs@gmail.com>
2026-03-09 23:46:47 -0300

    Removed prevent removal setting from ModConfig

--- src/main/java/com/mahghuuuls/combatinhibited/ModConfig.java
+++ src/main/java/com/mahghuuuls/combatinhibited/ModConfig.java
@@ -3,7 +3,6 @@ package com.mahghuuuls.combatinhibited;
 import com.mahghuuuls.combatinhibited.modules.dealingdamage.DealingDamageConfig;
 import com.mahghuuuls.combatinhibited.modules.nearboss.NearBossConfig;
 import com.mahghuuuls.combatinhibited.modules.nearenemy.NearEnemyConfig;
-import com.mahghuuuls.combatinhibited.modules.preventexpiring.PreventExpiringConfig;
 import com.mahghuuuls.combatinhibited.modules.takingdamage.TakingDamageConfig;
 import net.minecraftforge.common.config.Config;
 
@@ -25,8 +24,4 @@ public final class ModConfig {
     @Config.Name("near_enemy")
     @Config.Comment("Applies Inhibited while the player is within range of a configured enemy entity.")
     public static final NearEnemyConfig nearEnemyConfig = new NearEnemyConfig();
-
-    @Config.Name("prevent_expiring")
-    @Config.Comment("Prevents the Inhibited effect from expiring under configured conditions.")
-    public static final PreventExpiringConfig preventExpiringConfig = new PreventExpiringConfig();
 }
